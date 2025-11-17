4e74f1027855fee7f13bb1c39ff0917928d7d6e5
Mohammed Ashfaq Rahman <ashfaq.r05@gmail.com>
2025-11-17 14:36:47 +0000

    Create Level 3: Conditional Statements.sh

---
+++ BASH Battle Arena/Level 3: Conditional Statements.sh
@@ -0,0 +1,10 @@
+# Mission: Write a script that checks if a file named 'hero.txt' exists in the 'Arena' directory. If it does, print 'Hero found!'; otherwise, print 'Hero missing!'.
+
+
+#!/bin/bash
+
+if [ -f "Arena/hero.txt" ]; then
+    echo "Hero found!"
+else
+    echo "Hero missing!"
+fi
